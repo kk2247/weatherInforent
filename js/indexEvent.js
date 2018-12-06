4c8058d1de496ed2ae71b55312b5e003787d0e28
kk2247 <2247839764@qq.com>
2018-12-06 17:24:23 +0800

    添加用户与城市之间的关系及其功能

--- js/indexEvent.js
+++ js/indexEvent.js
@@ -24,7 +24,7 @@ window.onload=function(){
 	var account=getCookie("account");
 	var password=getCookie("password");
 	var state=getCookie("state");
-	var url="http://127.0.0.1:8099/weather/register?account="+account+"&password="+password;
+	var url="http://127.0.0.1:8099/weather/login?account="+account+"&password="+password;
 	if(state=="online"){
 		$.ajax({
             url:url,
@@ -33,12 +33,17 @@ window.onload=function(){
      		crossDomain: true,    
 			contentType: "application/json;charset=utf-8",
 			success:function (res) {
+				console.log(res);
 				var obj = eval(res);
 				console.log(obj);
 				if(obj.result=="true"){
 					setCookie("account",account,30);
 					setCookie("password",password,30);
 					setCookie("state","online",30);
+					var city="广州";
+					var temperature="10度~20度";
+					document.getElementById("temperature").innerHTML=temperature;
+					document.getElementById("city").innerHTML=city;
 				}else{
 					alert("请重新进行登录");
 　　					window.location.href="login.html";
@@ -56,3 +61,11 @@ window.onload=function(){
 	
 
 };
+
+function setCookie(c_name,value,expiredays)
+{
+var exdate=new Date()
+exdate.setDate(exdate.getDate()+expiredays)
+document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+return true;
+}
